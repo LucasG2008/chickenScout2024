c5622faf438743dd9b893311bf411cb943033a53
LucasG2008 <lucasgranucci08@gmail.com>
2024-01-25 15:03:59 -0600

    Added guest mode for offline sign-in

---
+++ chickenBotPie/Core/Authentication/View/GuestLoginView.swift
@@ -0,0 +1,18 @@
+//
+//  GuestLoginView.swift
+//  chickenBotPie
+//
+//  Created by Lucas Granucci on 1/23/24.
+//
+
+import SwiftUI
+
+struct GuestLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuestLoginView()
+}
---
+++ chickenBotPie/Core/Authentication/View/StartupView.swift
@@ -0,0 +1,18 @@
+//
+//  StartupView.swift
+//  chickenBotPie
+//
+//  Created by Lucas Granucci on 1/23/24.
+//
+
+import SwiftUI
+
+struct StartupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartupView()
+}
